5a9379976b9fce4a738b277d4ba5b8d600c8990a
RenatDavletshin <immortal1307@gmail.com>
2022-12-14 16:33:59 +0500

    Task3End

--- Task2.py
+++ Task2.py
@@ -1,18 +1,32 @@
 # Напишите программу, которая на вход принимает 5 чисел и находит максимальное их них
 
-num1 = int(input('Введите первое число: '))
-num2 = int(input('Введите второе число: '))
-num3 = int(input('Введите третье число: '))
-num4 = int(input('Введите четвертое число: '))
-num5 = int(input('Введите пятое число: '))
+# num1 = int(input('Введите первое число: '))
+# num2 = int(input('Введите второе число: '))
+# num3 = int(input('Введите третье число: '))
+# num4 = int(input('Введите четвертое число: '))
+# num5 = int(input('Введите пятое число: '))
 
-if num1 > num2 and num1 > num3 and num1 > num4 and num1 > num5:
-    print(f'{num1} максимальное')
-elif num2 > num1 and num2 > num3 and num2 > num4 and num2 > num5:
-    print(f'{num2} максимальное')
-elif num3 > num1 and num3 > num2 and num3 > num4 and num3 > num5:
-    print(f'{num3} максимальное')
-elif num4 > num1 and num4 > num2 and num4 > num3 and num4 > num5:
-    print(f'{num4} максимальное')
-else:
-    print(f'{num5} максимальное')
+# if num1 > num2 and num1 > num3 and num1 > num4 and num1 > num5:
+#     print(f'{num1} максимальное')
+# elif num2 > num1 and num2 > num3 and num2 > num4 and num2 > num5:
+#     print(f'{num2} максимальное')
+# elif num3 > num1 and num3 > num2 and num3 > num4 and num3 > num5:
+#     print(f'{num3} максимальное')
+# elif num4 > num1 and num4 > num2 and num4 > num3 and num4 > num5:
+#     print(f'{num4} максимальное')
+# else:
+#     print(f'{num5} максимальное')
+
+# numbers = []
+# for i in range (5):
+#     numbers.append(int(input(f'Введите число под номером {i+1}: '))) # используем функцию append - добавить конец
+# print(numbers)
+# print(max(numbers))
+
+
+my_max = 0
+for _ in range(5): # start, stop, step
+    num = int(input('Введите число: '))
+if my_max < num:
+    my_max = num
+print(my_max)
